9669160340e4dabf11753a66eb260d74a8db7eac
anniasebold <anniasebold3.0@gmail.com>
2022-05-30 01:02:12 -0300

    Leitura do arquivo em binário para String

--- disasm.java
+++ disasm.java
@@ -1,8 +1,5 @@
 import java.io.*;
 
-//
-// Não alterar o nome da classe!
-//
 class disasm {
   public static void main(String[] args) {
     if (args.length == 0)
@@ -14,9 +11,28 @@ class disasm {
     
     try ( InputStream inputStream = new FileInputStream(filename); )
     {
+
+      long fileSize = new File(filename).length();
+      byte[] allBytes = new byte[(int) fileSize];
+      String str[] = new String[allBytes.length];
+ 
+
+      int bytesRead = inputStream.read(allBytes);
+      
+      for(int i = 0; i < bytesRead; i++) {
+        str[i] = Character.toString(allBytes[i]);
+        System.out.print(str[i]);
         
-      // Aqui vem o seu código!
-        
+        // if(i % 4 == 0) {
+        //   String bytes[] = new String[4];
+        //   for(int y = 0; y < bytes.length; y++) {
+        //     bytes[y] = str[y];
+        //     System.out.print(bytes[y]);
+        //     // str[y] = null;
+        //   }
+        // }
+      }
+      
     } catch (IOException ex) {
       ex.printStackTrace();
     }
