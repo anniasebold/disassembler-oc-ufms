f556ccbe101ba538b2c70fc39e1ea028c8410a36
anniasebold <anniasebold3.0@gmail.com>
2022-06-01 00:54:01 -0300

    Interpretando bytes e imprimindo instrução correspondente

--- disasm.java
+++ disasm.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 
 class disasm {
   public static void main(String[] args) {
@@ -22,14 +23,63 @@ class disasm {
         str += Character.toString(allBytes[i]);
       }
       str = str.replace(" ", "");
-      System.out.println(str);
+      if(str.contains("4") || 
+        str.contains("3") || 
+        str.contains("B") || 
+        str.contains("A") ||
+        str.contains("2") ||
+        str.contains("5") ||
+        str.contains("6") ||
+        str.contains("9") ||
+        str.contains("8") ||
+        str.contains("C") ||
+        str.contains("D") ||
+        str.contains("E") ||
+        str.contains("7")) {
+      } else {
+        str = new BigInteger(str, 2).toString(16);
+      }
+      
+      Character bytes[] = new Character[4];
 
-      for(int i = 0; i < str.length(); i++) {
+      for(int i = 0; i <= str.length(); i++) {
         if(i % 4 == 0 && i != 0) {
-          System.out.println(str.charAt(i));
-          System.out.println(str.charAt(i - 1));
-          System.out.println(str.charAt(i - 2));
-          System.out.println(str.charAt(i - 3));
+          bytes[0] = str.charAt(i - 4);
+          bytes[1] = str.charAt(i - 3);
+          bytes[2] = str.charAt(i - 2);
+          bytes[3] = str.charAt(i - 1);
+
+          if(bytes[0].equals('3')) {
+            System.out.print("Add " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('4')) {
+            System.out.print("Subt " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('B')) {
+            System.out.print("AddI " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('A')) {
+            System.out.print("Clear");
+          } else if(bytes[0].equals('1')) {
+            System.out.print("Load " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('2')) {
+            System.out.print("Store " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('5')) {
+            System.out.print("Input");
+          } else if(bytes[0].equals('6')) {
+            System.out.print("Output");
+          } else if(bytes[0].equals('9')) {
+            System.out.print("Jump " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('8')) {
+            System.out.print("Skipcond");
+          } else if(bytes[0].equals('0')) {
+            System.out.print("JnS " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('C')) {
+            System.out.print("jumpl " + bytes[1] + bytes[2] + bytes[3] + "\n");
+          } else if(bytes[0].equals('D')) {
+            System.out.print("LoadI");
+          } else if(bytes[0].equals('E')) {
+            System.out.print("StoreI");
+          } else if(bytes[0].equals('7')) {
+            System.out.print("Halt");
+          }
         }
       }
       
